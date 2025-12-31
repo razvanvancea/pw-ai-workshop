565903e0bdd9327c8bc390f0d0ae7faa68d6a3bb
Razvan Vancea <razvanvancea94@gmail.com>
2025-12-31 17:31:01 +0200

    add logout scenario - codegen raw

--- tests/user-authentication.spec.ts
+++ tests/user-authentication.spec.ts
@@ -7,3 +7,17 @@ test('login test', async ({ page }) => {
   await page.locator('[data-test="login-button"]').click();
   await expect(page.locator('[data-test="title"]')).toBeVisible();
 });
+
+test('logout test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('div').nth(4).click();
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').press('Tab');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await expect(page.locator('[data-test="logout-sidebar-link"]')).toContainText('Logout');
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+  await expect(page.locator('[data-test="login-button"]')).toBeVisible();
+});
