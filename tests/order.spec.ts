d0ecc472b876aec4057dec0ac862535201145046
Razvan Vancea <razvanvancea94@gmail.com>
2025-12-31 17:44:17 +0200

    add to cart test complete

--- tests/order.spec.ts
+++ tests/order.spec.ts
@@ -1,6 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('add to cart test', async ({ page }) => {
+test('add to cart test @de', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   // use the debugger and start the 'add to cart' test from here
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  //   await page.pause();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await expect(page.locator('[data-test="continue-shopping"]')).toBeVisible();
+  await expect(page.locator('[data-test="checkout"]')).toBeVisible();
+  await expect(page.locator('[data-test="remove-sauce-labs-backpack"]')).toBeVisible();
 });
