0a7aeab2e36bc22fdf104cb70035410670b41d3e
Razvan Vancea <razvanvancea94@gmail.com>
2025-12-31 15:12:40 +0200

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,10 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 60_000,
+  expect: {
+    timeout: 15_000,
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -22,14 +26,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure', // 'on-first-retry', 'on-all-retries', 'off', 'on' https://trace.playwright.dev
+    video: 'off', // 'off' is the default, 'on', 'on-first-retry', 'retain-on-failure',
+    screenshot: 'only-on-failure', // on, off,
   },
 
   /* Configure projects for major browsers */
