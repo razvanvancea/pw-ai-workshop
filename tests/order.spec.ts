c5a7e7de051bc605b9e5932dc6aa0af5b3ad8122
Razvan Vancea <razvanvancea94@gmail.com>
2025-12-31 17:44:52 +0200

    remove tag

--- tests/order.spec.ts
+++ tests/order.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('add to cart test @de', async ({ page }) => {
+test('add to cart test', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   // use the debugger and start the 'add to cart' test from here
   await page.locator('[data-test="username"]').fill('standard_user');
