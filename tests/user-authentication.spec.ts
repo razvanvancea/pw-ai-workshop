0132c854a63298ed2560fae0912fefb05bc058c6
Razvan Vancea <razvanvancea94@gmail.com>
2025-12-31 17:34:48 +0200

    remove empty space

--- tests/user-authentication.spec.ts
+++ tests/user-authentication.spec.ts
@@ -11,7 +11,6 @@ test('login test', async ({ page }) => {
 test('logout test', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').fill('standard_user');
-
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
   await page.getByRole('button', { name: 'Open Menu' }).click();
