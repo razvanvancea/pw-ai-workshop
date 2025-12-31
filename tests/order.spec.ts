9ef852f4d484e1df7e40c7e256f9f505532d3736
Razvan Vancea <razvanvancea94@gmail.com>
2025-12-31 17:39:40 +0200

    add to cart test starter

---
+++ tests/order.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('add to cart test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  // use the debugger and start the 'add to cart' test from here
+});
