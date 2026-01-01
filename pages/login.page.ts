4ab19d65ea2e5e21978fecee363670b6d9a37163
Razvan Vancea <razvanvancea94@gmail.com>
2026-01-01 12:14:14 +0200

    add login pom

---
+++ pages/login.page.ts
@@ -0,0 +1,24 @@
+import { expect, type Locator, type Page } from "@playwright/test";
+
+export class LoginPage {
+  readonly page: Page;
+  readonly emailInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+  readonly productsHeaderText: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.emailInput = page.locator('[data-test="username"]');
+    this.passwordInput = page.locator('[data-test="password"]');
+    this.loginButton = page.locator('[data-test="login-button"]');
+    this.productsHeaderText = page.locator('[data-test="title"]');
+  }
+
+  async doLogin(email: string, password: string) {
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+    await expect(this.productsHeaderText).toBeVisible();
+  }
+}
--- tests/user-authentication.spec.ts
+++ tests/user-authentication.spec.ts
@@ -1,20 +1,18 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
 test.describe('user authentication test suite', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
   });
 
   test('login test', async ({ page }) => {
-    await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
-    await expect(page.locator('[data-test="title"]')).toBeVisible();
+    const loginPage = new LoginPage(page);
+    await loginPage.doLogin('standard_user','secret_sauce');
   });
 
   test('logout test', async ({ page }) => {
-    await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
+    const loginPage = new LoginPage(page);
+    await loginPage.doLogin('standard_user','secret_sauce');
     await page.getByRole('button', { name: 'Open Menu' }).click();
     await expect(page.locator('[data-test="logout-sidebar-link"]')).toContainText('Logout');
     await page.locator('[data-test="logout-sidebar-link"]').click();
