366fdd3ffaa3de9f8420e494512737425e634dec
Razvan Vancea <razvanvancea94@gmail.com>
2026-01-01 11:46:44 +0200

    add describe and beforeEach

--- tests/user-authentication.spec.ts
+++ tests/user-authentication.spec.ts
@@ -1,20 +1,23 @@
 import { test, expect } from '@playwright/test';
+test.describe('user authentication test suite', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+  });
 
-test('login test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await expect(page.locator('[data-test="title"]')).toBeVisible();
-});
+  test('login test', async ({ page }) => {
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page.locator('[data-test="title"]')).toBeVisible();
+  });
 
-test('logout test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await page.getByRole('button', { name: 'Open Menu' }).click();
-  await expect(page.locator('[data-test="logout-sidebar-link"]')).toContainText('Logout');
-  await page.locator('[data-test="logout-sidebar-link"]').click();
-  await expect(page.locator('[data-test="login-button"]')).toBeVisible();
+  test('logout test', async ({ page }) => {
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await page.getByRole('button', { name: 'Open Menu' }).click();
+    await expect(page.locator('[data-test="logout-sidebar-link"]')).toContainText('Logout');
+    await page.locator('[data-test="logout-sidebar-link"]').click();
+    await expect(page.locator('[data-test="login-button"]')).toBeVisible();
+  });
 });
