80f5d12451385bca3ad9a3da9c6c133d6fe9f6d2
Razvan Vancea <razvanvancea94@gmail.com>
2026-02-23 00:26:21 +0200

    add docs for faker

--- tests/user-authentication.spec.ts
+++ tests/user-authentication.spec.ts
@@ -8,5 +8,6 @@ test('login test', async ({ page }) => {
   await page.locator('[data-test="login-button"]').click();
   await expect(page.locator('[data-test="title"]')).toBeVisible();
 
+  // Docs: https://fakerjs.dev/api/
   console.log(faker.person.fullName());
 });
