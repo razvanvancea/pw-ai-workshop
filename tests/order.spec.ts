e3470d8f7059be7d5e7e0c550415cc933fda2354
Razvan Vancea <razvanvancea94@gmail.com>
2026-02-27 18:38:46 +0200

    refactor test

--- tests/order.spec.ts
+++ tests/order.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('add to cart test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  // use the debugger and start the 'add to cart' test from here
+test.describe('User authentication test suite', () => {
+  
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://tai-shop.razvanvancea.ro/');
+  });
+
+  test('add to cart test', async ({ page }) => {
+    // use the debugger and start the 'add to cart' test from here
+  });
 });
