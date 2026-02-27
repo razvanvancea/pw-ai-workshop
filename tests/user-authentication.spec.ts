096210457ea09b49b6f3740d26926f59420689e9
Razvan Vancea <razvanvancea94@gmail.com>
2026-02-27 09:24:26 +0200

    add custom test.step

--- tests/user-authentication.spec.ts
+++ tests/user-authentication.spec.ts
@@ -16,6 +16,7 @@ test.describe('User authentication test suite', () => {
     await page.locator('[data-test="username"]').fill('standard_user');
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[data-test="login-button"]').click();
+
     await test.step('Perform logout action', async () => {
       await page.getByRole('button', { name: 'Open Menu' }).click();
       await expect(page.locator('[data-test="logout-sidebar-link"]')).toContainText('Logout');
