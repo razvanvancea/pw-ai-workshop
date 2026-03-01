a34102737a0ed13ddd032d6e9f652829782ea6f4
Razvan Vancea <razvanvancea94@gmail.com>
2026-03-01 11:18:33 +0200

    update test

--- tests/order.spec.ts
+++ tests/order.spec.ts
@@ -11,7 +11,7 @@ test.describe('User authentication test suite', () => {
     await page.getByRole('textbox', { name: 'Email Address' }).fill('admin@admin.com');
     await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
     await page.getByTestId('submitBtn').click();
-    await page.getByRole('link', { name: 'Log Out' }).click();
-    await expect(page.getByRole('heading', { name: 'Welcome back' })).toBeVisible();
+    await page.getByRole('button', { name: 'ADD TO CART' }).first().click();
+    await expect(page.getByRole('button', { name: 'REMOVE' })).toBeVisible();
   });
 });
