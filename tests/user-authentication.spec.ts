a2980f296af8cde2990ade09493efec3b250d126
Razvan Vancea <razvanvancea94@gmail.com>
2026-03-02 13:22:50 +0200

    update custom step

--- tests/user-authentication.spec.ts
+++ tests/user-authentication.spec.ts
@@ -6,11 +6,13 @@ test.describe('User authentication test suite', () => {
   });
 
   test('login test', async ({ page }) => {
-    await test.step('Fill login form', async () => {
-      await page.getByRole('textbox', { name: 'Email Address' }).fill('admin@admin.com');
-      await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
-    });
+    await page.getByRole('textbox', { name: 'Email Address' }).fill('admin@admin.com');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+
     await page.getByTestId('submitBtn').click();
-    await expect(page.getByRole('link', { name: 'Log Out' })).toBeVisible();
+
+    await test.step('Verify logout link is visible', async () => {
+      await expect(page.getByRole('link', { name: 'Log Out' })).toBeVisible();
+    });
   });
 });
